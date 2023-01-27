495d2db08854333640576fec9f6ee7374b9e1c15
Rajm1505 <95242392+Rajm1505@users.noreply.github.com>
2023-01-28 00:18:06 +0530

    Celery added, Mail Replies and Body APIs and Basic Frontend

--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
-from urllib import response
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from decouple import config
 
-from .models import Recipients
+from .models import Departments, Recipients
 
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
@@ -10,47 +10,186 @@ from rest_framework.decorators import api_view
 from django.views.decorators.csrf import csrf_exempt
 from django.core.mail import send_mass_mail, send_mail
 
+import easyimap as e
 
-# Create your views here.
+from .tasks import sendMailWorker
 
+import datetime
 
 def index(request):
-    return HttpResponse('This is api index')
+    return HttpResponse('Use Appropriate path')
 
 @api_view(['POST'])
-# @csrf_exempt
+@csrf_exempt
 def sendMail(request):
     if request.method == 'POST':
+        # Parsing json string
         data = JSONParser().parse(request)
-        message = data['message']
-        department = data['department']
-
-        subject = "Hardik to Hardik che"
-
-        reciepents = Recipients.objects.filter(department = 'Hospitals')
-
-        reciepentlist = reciepentstolist(reciepents)
-        
-        print('list' ,reciepentlist)
-        res = send_mail(subject, message,'automailclient0@gmail.com', reciepentlist,fail_silently = False)
-        # res = send_mass_mail((subject, message, 'automailclient0@gmail.com', reciepentlist),fail_silently = False)
-
-
-        print(res)
-        # print(request.data)
-        # message = request.data['message']
         response = Response()
+        message = data['message']
+        subject = data['subject']
+
+        # Checking if department exists
+        try:
+            dept = Departments.objects.get(name = data["department"])
+        except:
+            print("Dept not found!")
+            response.data = {
+                "Sent" : 0,
+                "message" : "Department not found!"
+            }
+            return response 
+
+        
+        # Getting reciepent's emails in a list
+        reciepents = Recipients.objects.filter(did = dept)
+        if reciepents:
+            reciepentlist = reciepentstolist(reciepents)
+            print('list' ,reciepentlist)
+        else:
+            print("Reciepents not found!")
+            response.data = {
+                "Sent" : 0,
+                "message" : f"There are no Reciepents in {dept}"
+            }
+            return response 
+
+        # Sends mail through a celery worker
+        sendMailWorker.delay(subject,message,reciepentlist)
+
         response.data = {
-            'Sent' : res
+            'Sent' : 1
         }
         return response
 
 def reciepentstolist(reciepents):
     reciepentlist = []
-    for i in reciepents :
-        reciepentlist.append(i.email)
+    for rec in reciepents :
+        reciepentlist.append(rec.email)
     return reciepentlist
 
+@api_view(['GET'])
+def newMailReplies(request):
+    if request.method == "GET":
+
+        start = datetime.datetime.now()
+        response = Response()
+        mailserver = e.connect("imap.gmail.com", config("EMAIL_HOST_USER"),config("EMAIL_HOST_PASSWORD"))
+
+        # Returns mailobjects of all the unseen mails
+        unseen_mails = mailserver.unseen(20)
+        if not unseen_mails:
+            print("no mails")
+            response.status_code = 404
+            response.data = {
+                'message': 'No unseen mails Found'
+            }
+            return response
+        print("Unseen Mails: ",unseen_mails)
+
+        mails = {}
+        counter = 1
+        for i in range(0,len(unseen_mails)):
+            if 'Re:' in unseen_mails[i].title:  
+                print("found")
+                mails[counter] = {}
+                mails[counter]['id'] = str(unseen_mails[i].uid).encode()
+                mails[counter]['title'] = unseen_mails[i].title 
+                mails[counter]['from_addr'] = unseen_mails[i].from_addr
+                counter+=1
+              
+        response.data = mails
+
+        print("response", response.data)
+        end = datetime.datetime.now()
+        print("Time taken: ",end-start)
+        return response
+
+        # Another Approach
+        # start = datetime.datetime.now()
+        # mailids = server.listids(limit=20)
+        # print(mailids)
+        # response = Response()
+
+        # mail_titles = {}
+        # for i in range(0,len(mailids)):
+        #     email = server.mail(mailids[i], include_raw=True)
+        #     if 'Re:' in email.title:
+        #         mail_titles[i] = email.title 
+            
+        #     response.data = mail_titles
+        # print("response", response.data)
+        # end = datetime.datetime.now()
+        # print("Time taken: ",end-start)
+        # return response
+
+
+@api_view(['GET'])
+def allMailReplies(request):
+    if request.method == "GET":
+        start = datetime.datetime.now()
+
+        response = Response()
+        server = e.connect("imap.gmail.com", config("EMAIL_HOST_USER"),config("EMAIL_HOST_PASSWORD"))
+        all_mailids = server.listids(20)
+        
+        print("Mailids: ",all_mailids)
+        if not all_mailids:
+            print("no mails")
+            response.status_code = 404
+            response.data = {
+                'message': 'No mails Found'
+            }
+            return response
+        
+        mails = {}
+        counter = 1
+        for i in range(0,len(all_mailids)):
+            email = server.mail(all_mailids[i],include_raw=True)
+            if 'Re:' in email.title:  
+                mails[counter] = {}
+                print("found")
+                mails[counter]['id'] = str(email.uid).encode()
+                mails[counter]['title'] = email.title 
+                mails[counter]['from_addr'] = email.from_addr
+                counter+=1
+              
+        response.data = mails
+
+        print("response", response.data)
+        end = datetime.datetime.now()
+        print("Time taken: ",end-start)
+        return response
+    
+
+@api_view(['POST'])
+def mailBody(request):
+    # mid = request.GET.get('mid')
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        id = data['id']
+        mailserver = e.connect("imap.gmail.com", config("EMAIL_HOST_USER"),config("EMAIL_HOST_PASSWORD"))
+        email = mailserver.mail(id,include_raw = True)
+        print(email)
+        response = Response()
+        print((email.body).split('\n')[0])
+
+        if email:
+            response.status_code = 200
+            response.data = {
+                'date' : email.date,
+                'title': email.title,
+                'from_addr': email.from_addr,
+                'body' : email.body
+            }
+            return response 
+        else:
+            response.status_code = 404
+            response.data = {
+                'message' : 'Email not found!'
+            }
+            return response 
+        
 
 
 
---
+++ src/pages/MailReplies.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import LoadingSpinner from "../Components/spinner/LoadingSpinner";
+
+
+
+import axios from "axios";
+// import { useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+
+const MailReplies = () => {
+    const [MailReplies,setMailReplies] = useState([]);
+    const [loading, setLoading] = useState();
+
+
+    const allReplies = ()=>{
+        setLoading(true)
+        const options = {method: 'GET', url: 'http://127.0.0.1:8000/api/mailreplies/'};
+            
+             axios.request(options).then(function (response) {
+                console.log(response.data);
+                setMailReplies(Object.values(response.data))
+                setLoading(false);
+            }).catch( (err)=> {
+                console.error(err);
+            });
+        }
+    const unseenReplies = ()=>{
+        setLoading(true)
+        const options = {method: 'GET', url: 'http://127.0.0.1:8000/api/mailreplies/new'};
+            
+             axios.request(options).then(function (response) {
+                console.log(response.data);
+                setMailReplies(Object.values(response.data) );
+                setLoading(false);
+            }).catch(function (error) {
+                console.error(error);
+                setLoading(false);
+            
+            });
+        }
+    
+        
+        
+        
+        return (
+            <>
+                
+            <div className="container m-auto mt-5">
+
+            <Button className="btn btn-primary me-5" onClick={unseenReplies}>Load Unseen Replies</Button>
+            <Button className="btn btn-success" onClick={allReplies}>Load All Replies</Button>
+            {loading ? (
+                    <LoadingSpinner/>
+            ) : (
+                <div>
+                    {MailReplies.map((reply, index) => (
+                        <div key={index}>
+                            <h3 className="text-light">{reply.title}</h3>
+                            <p className="text-light">{reply.from_addr}</p>
+                            <a href={`/mailbody?id=${reply.id}`} className="btn btn-warning">View Mail</a>
+                            <hr />
+                        </div>
+                    ))}
+                </div>
+            )}
+            </div>  
+
+            </>
+  );
+};
+
+
+export default MailReplies;
